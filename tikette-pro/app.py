6527f62350025e017b517d2872e3c9d7f982dc49
dvdmontagut <60674652+dvdmontagut@users.noreply.github.com>
2023-01-05 15:22:36 +0100

    Latitudes Longitudes
    Se añade latitud y longitud a los datos

--- tikette-pro/app.py
+++ tikette-pro/app.py
@@ -18,30 +18,39 @@ def search():
     dic = request.form
     tam = len(dic)
     if tam == 1:
-        if "artist" in dic:
-            argumentos = {'classificationName': 'music', 'keyword': dic['artist'],
-                          'apikey': 'HNYtWPE0DGcjUFwmi0FBPTpbrZ3p7znK'}
-            url = f"https://app.ticketmaster.com/discovery/v2/events.json"
-            r = requests.get(url, params=argumentos)
-            response = json.loads(r.text)
-
-            cuantos = 20 if response['page']['totalElements'] > 20 else response['page']['totalElements']
-            nombres = {}
-            fotos = {}
-            fechas = {}
-            horas = {}
-            links = {}
-            for i in range(cuantos):
-                # response['_embedded']['events'][i]
-                nombres[i] = response['_embedded']['events'][i]['name']
-                fotos[i] = response['_embedded']['events'][i]['images'][0]['url']
-                fechas[i] = response['_embedded']['events'][i]['dates']['start']['localDate']
-                horas[i] = response['_embedded']['events'][i]['dates']['start']['localTime']
-                links[i] = response['_embedded']['events'][i]['url']
-            return render_template('NameSearch.html', cuantos=range(cuantos), nombres=nombres, fotos=fotos, fechas=fechas,
-                                   horas=horas, links=links)
-        else:
+        if "artist" not in dic:
             return render_template('NameSearch.html')
+
+        argumentos = {'classificationName': 'music', 'keyword': dic['artist'],
+                      'apikey': 'HNYtWPE0DGcjUFwmi0FBPTpbrZ3p7znK'}
+        url = f"https://app.ticketmaster.com/discovery/v2/events.json"
+        r = requests.get(url, params=argumentos)
+        response = json.loads(r.text)
+
+        cuantos = 20 if response['page']['totalElements'] > 20 else response['page']['totalElements']
+        nombres = {}
+        fotos = {}
+        fechas = {}
+        horas = {}
+        links = {}
+        latitudes = {}
+        longitudes = {}
+        for i in range(cuantos):
+            # response['_embedded']['events'][i]
+            nombres[i] = response['_embedded']['events'][i]['name']
+            fotos[i] = response['_embedded']['events'][i]['images'][0]['url']
+            fechas[i] = response['_embedded']['events'][i]['dates']['start']['localDate']
+            if "localTime" in response['_embedded']['events'][i]['dates']['start']:
+                horas[i] = response['_embedded']['events'][i]['dates']['start']['localTime']
+            else:
+                horas[i] = "---"
+            latitudes[i] = response['_embedded']['events'][i]['_embedded']['venues'][0]['location']['latitude']
+            longitudes[i] = response['_embedded']['events'][i]['_embedded']['venues'][0]['location']['longitude']
+            links[i] = response['_embedded']['events'][i]['url']
+        return render_template('NameSearch.html', cuantos=range(cuantos), nombres=nombres, fotos=fotos, fechas=fechas,
+                               horas=horas, links=links,latitudes=latitudes, longitudes=longitudes)
+    else:
+        return render_template('NameSearch.html')
 
 
 if __name__ == "__main__":
